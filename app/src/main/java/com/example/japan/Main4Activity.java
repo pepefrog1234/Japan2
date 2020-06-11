46ae6b835649a8e74be452fde05b7d85aa19685a
yoyo3 <yoyo3618180@gmail.com>
2020-06-11 18:54:34 +0800

    123

--- app/src/main/java/com/example/japan/Main4Activity.java
+++ app/src/main/java/com/example/japan/Main4Activity.java
@@ -13,18 +13,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Main4Activity extends AppCompatActivity{
-    Button back,clear,next,front;
-    TextView t,c,e,f,slow,avg,y,test;
+    Button back,clear,enter;
+    TextView t,c,e,f,slow,avg,y;
+    EditText search;
     final FirebaseDatabase database=FirebaseDatabase.getInstance();
     DatabaseReference data=database.getReference("data");
     int dat=1;
+    int change;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        search=findViewById(R.id.editTextTextPersonName);
+        enter=findViewById(R.id.button11);
         t=findViewById(R.id.textView15);
         c=findViewById(R.id.textView17);
         e=findViewById(R.id.textView18);
@@ -32,30 +37,7 @@ public class Main4Activity extends AppCompatActivity{
         f=findViewById(R.id.textView20);
         slow=findViewById(R.id.textView21);
         y=findViewById(R.id.textView16);
-        test=findViewById(R.id.textView22);
-        next=findViewById(R.id.next);
-        front=findViewById(R.id.front);
         back=findViewById(R.id.button9);
-        next.setOnClickListener(new Button.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                dat++;
-                /*if(dat>fin){
-                    dat--;
-                }*/
-            }
-        });
-        front.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dat--;
-                if(dat<1){
-                    dat++;
-                }
-            }
-        });
-        test.setText("test:"+dat);
         back.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,32 +59,21 @@ public class Main4Activity extends AppCompatActivity{
             @Override
             public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
                 final int fin=(int)dataSnapshot.getChildrenCount();
-                next.setOnClickListener(new Button.OnClickListener() {
-
+                enter.setOnClickListener(new Button.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        dat++;
-                        if(dat>fin){
-                            dat--;
+                        change=Integer.parseInt(search.getText().toString());
+                        if(!("").equals(search.getText().toString())||fin>change){
+                            t.setText("總答題數:"+dataSnapshot.child(String.valueOf(change)).child("total").getValue());
+                            c.setText("總答對題數:"+dataSnapshot.child(String.valueOf(change)).child("correct").getValue());
+                            e.setText("總答錯題數:"+dataSnapshot.child(String.valueOf(change)).child("error").getValue());
+                            f.setText("最快反應時間:"+dataSnapshot.child(String.valueOf(change)).child("fast").getValue());
+                            slow.setText("最慢反應時間:"+dataSnapshot.child(String.valueOf(change)).child("slow").getValue());
+                            avg.setText("平均反應時間:"+dataSnapshot.child(String.valueOf(change)).child("avgtime").getValue());
+                            y.setText("答對率:"+Math.rint(Double.parseDouble(String.valueOf(dataSnapshot.child(String.valueOf(change)).child("yy").getValue()))*100)/100+"%");
                         }
                     }
                 });
-                front.setOnClickListener(new Button.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dat--;
-                        if(dat<1){
-                            dat++;
-                        }
-                    }
-                });
-                t.setText("總答題數:"+dataSnapshot.child(String.valueOf(dat)).child("total").getValue());
-                c.setText("總答對題數:"+dataSnapshot.child(String.valueOf(dat)).child("correct").getValue());
-                e.setText("總答錯題數:"+dataSnapshot.child(String.valueOf(dat)).child("error").getValue());
-                f.setText("最快反應時間:"+dataSnapshot.child(String.valueOf(dat)).child("fast").getValue());
-                slow.setText("最慢反應時間:"+dataSnapshot.child(String.valueOf(dat)).child("slow").getValue());
-                avg.setText("平均反應時間:"+dataSnapshot.child(String.valueOf(dat)).child("avgtime").getValue());
-                y.setText("答對率:"+dataSnapshot.child(String.valueOf(dat)).child("yy").getValue());
             }
 
             @Override
